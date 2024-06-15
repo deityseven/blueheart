77f905820811a382705455961c35e91428b73e35
deityseven <3243881379@qq.com>
2024-06-15 13:12:26 +0800

    完善压缩器

--- src/projects/server/executableprogram/ziper/ziper.go
+++ src/projects/server/executableprogram/ziper/ziper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"encoding/json"
 	"io"
 	"os"
 )
@@ -12,18 +13,21 @@ type ZipData struct {
 	JsonData string `json:"jsonData"`
 }
 
-func (d *Response) Zip(filePath string, data string) {
+func (d *Response) Zip(input string) {
+
+	var zipData ZipData
+	json.Unmarshal([]byte(input), &zipData)
 
 	buf := new(bytes.Buffer)
 	zipWriter := zip.NewWriter(buf)
 
-	zipFile, err := zipWriter.Create(filePath)
+	zipFile, err := zipWriter.Create(zipData.FilePath)
 	if err != nil {
 		d.Msg = "zip create failed"
 		d.Success = false
 	}
 
-	_, err = io.WriteString(zipFile, data)
+	_, err = io.WriteString(zipFile, zipData.JsonData)
 	if err != nil {
 		d.Msg = "zip write failed"
 		d.Success = false
@@ -35,24 +39,37 @@ func (d *Response) Zip(filePath string, data string) {
 		d.Success = false
 	}
 
-	os.WriteFile(filename+".zip", buf.Bytes(), 0644)
+	os.WriteFile(zipData.FilePath, buf.Bytes(), 0644)
 
-	if len(users) >= 1 {
-		d.Msg = "The phone number is registered"
-		d.Success = false
-	} else {
-		d.Msg = "The phone number isn't registered"
-		d.Success = true
-	}
+	d.Msg = "zip to " + zipData.FilePath
+	d.Success = true
 }
 
-func (d *Response) Unzip(filePath string, data string) {
+func (d *Response) Unzip(input string) {
+	var zipData ZipData
+	json.Unmarshal([]byte(input), &zipData)
 
-	if result.Error != nil {
-		d.Msg = "add user faild"
+	reader, err := zip.OpenReader(zipData.FilePath)
+	if err != nil {
+		d.Msg = "zip open failed"
 		d.Success = false
-	} else {
-		d.Msg = "add user success"
-		d.Success = true
 	}
+	defer reader.Close()
+
+	firstFile := reader.Reader.File[0]
+	firstFileReader, err := firstFile.Open()
+	if err != nil {
+		d.Msg = "zip context error"
+		d.Success = false
+	}
+	defer firstFileReader.Close()
+
+	content, err := io.ReadAll(firstFileReader)
+	if err != nil {
+		d.Msg = "zip file context error"
+		d.Success = false
+	}
+
+	d.Msg = string(content)
+	d.Success = true
 }
